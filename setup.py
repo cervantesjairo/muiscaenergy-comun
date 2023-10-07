18b8262de9c0ba4ed49e03b3b81d10dfa9536e85
jc <jhcervantesg@gmail.com>
2023-10-07 10:51:04 -0400

    rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
     setup(
         name='muiscaenergy_comun',
-        version='0.0.5',
+        version='0.0.6',
         url='https://pypi.org/project/',
         author='Jairo Hernando Cervantes Garcia',
         emai='muiscaenergy@gmail.com',
